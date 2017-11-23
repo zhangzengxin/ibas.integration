c43b251d300d5554997560a4f02fddc799331d44
牛加人等于朱 <niuren.zhu@icloud.com>
2017-11-23 17:02:32 +0800

    添加版本信息

--- ibas.integration.service/src/main/webapp/bsapp/Console.ts
+++ ibas.integration.service/src/main/webapp/bsapp/Console.ts
@@ -15,11 +15,14 @@ export class Console extends ibas.ModuleConsole {
     static CONSOLE_ID: string = "568e7e6d-a67a-46a8-93b9-7bc896b1c2b7";
     /** 模块-名称 */
     static CONSOLE_NAME: string = "Integration";
+    /** 模块-版本 */
+    static CONSOLE_VERSION: string = "0.1.0";
     /** 构造函数 */
     constructor() {
         super();
         this.id = Console.CONSOLE_ID;
         this.name = Console.CONSOLE_NAME;
+        this.version = Console.CONSOLE_VERSION;
     }
     private _navigation: ibas.IViewNavigation;
     /** 创建视图导航 */
