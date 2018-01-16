7a196ee3deef5908ced5c94c6aa7e23c55235cb5
牛加人等于朱 <niuren.zhu@icloud.com>
2018-01-16 14:28:54 +0800

    调整物料批次、序列号相关

--- ibas.integration/src/main/java/org/colorcoding/ibas/integration/bo/integrationjob/IntegrationJobActions.java
+++ ibas.integration/src/main/java/org/colorcoding/ibas/integration/bo/integrationjob/IntegrationJobActions.java
@@ -1,79 +1,82 @@
 package org.colorcoding.ibas.integration.bo.integrationjob;
 
 import java.beans.PropertyChangeEvent;
-import javax.xml.bind.annotation.*;
-import org.colorcoding.ibas.bobas.common.*;
-import org.colorcoding.ibas.bobas.bo.*;
+
+import javax.xml.bind.annotation.XmlSeeAlso;
+import javax.xml.bind.annotation.XmlType;
+
+import org.colorcoding.ibas.bobas.bo.BusinessObjects;
+import org.colorcoding.ibas.bobas.common.ICriteria;
 import org.colorcoding.ibas.integration.MyConfiguration;
 
 /**
-* 集成任务-动作 集合
-*/
+ * 集成任务-动作 集合
+ */
 @XmlType(name = IntegrationJobActions.BUSINESS_OBJECT_NAME, namespace = MyConfiguration.NAMESPACE_BO)
 @XmlSeeAlso({ IntegrationJobAction.class })
-public class IntegrationJobActions extends BusinessObjects<IIntegrationJobAction, IIntegrationJob> implements IIntegrationJobActions {
+public class IntegrationJobActions extends BusinessObjects<IIntegrationJobAction, IIntegrationJob>
+		implements IIntegrationJobActions {
 
-    /**
-    * 业务对象名称
-    */
-    public static final String BUSINESS_OBJECT_NAME = "IntegrationJobActions";
+	/**
+	 * 业务对象名称
+	 */
+	public static final String BUSINESS_OBJECT_NAME = "IntegrationJobActions";
 
-    /**
-     * 序列化版本标记
-     */
-    private static final long serialVersionUID = 6041189511831305116L;
+	/**
+	 * 序列化版本标记
+	 */
+	private static final long serialVersionUID = 6041189511831305116L;
 
-    /**
-     * 构造方法
-     */
-    public IntegrationJobActions() {
-        super();
-    }
+	/**
+	 * 构造方法
+	 */
+	public IntegrationJobActions() {
+		super();
+	}
 
-    /**
-     * 构造方法
-     * @param parent 父项对象
-     */
-    public IntegrationJobActions(IIntegrationJob parent) {
-        super(parent);
-    }
+	/**
+	 * 构造方法
+	 * 
+	 * @param parent
+	 *            父项对象
+	 */
+	public IntegrationJobActions(IIntegrationJob parent) {
+		super(parent);
+	}
 
-    /**
-     * 元素类型
-     */
-    public Class<?> getElementType() {
-        return IntegrationJobAction.class;
-    }
+	/**
+	 * 元素类型
+	 */
+	public Class<?> getElementType() {
+		return IntegrationJobAction.class;
+	}
 
-    /**
-    * 创建集成任务-动作
-    * 
-    * @return 集成任务-动作
-    */
-    public IIntegrationJobAction create() {
-        IIntegrationJobAction item = new IntegrationJobAction();
-        if (this.add(item)) {
-            return item;
-        }
-        return null;
-    }
+	/**
+	 * 创建集成任务-动作
+	 * 
+	 * @return 集成任务-动作
+	 */
+	public IIntegrationJobAction create() {
+		IIntegrationJobAction item = new IntegrationJobAction();
+		if (this.add(item)) {
+			return item;
+		}
+		return null;
+	}
 
-    @Override
-    protected void afterAddItem(IIntegrationJobAction item) {
-        super.afterAddItem(item);
-        // TODO 设置关联值
-    }
+	@Override
+	protected void afterAddItem(IIntegrationJobAction item) {
+		super.afterAddItem(item);
+	}
 
-    @Override
-    public ICriteria getElementCriteria() {
-        ICriteria criteria = super.getElementCriteria();
-        // TODO 添加关联查询条件
-        return criteria;
-    }
+	@Override
+	public ICriteria getElementCriteria() {
+		ICriteria criteria = super.getElementCriteria();
+		return criteria;
+	}
 
-    @Override
-    public void onParentPropertyChanged(PropertyChangeEvent evt) {
-        super.onParentPropertyChanged(evt);
-        // TODO 设置关联值
-    }
+	@Override
+	public void onParentPropertyChanged(PropertyChangeEvent evt) {
+		super.onParentPropertyChanged(evt);
+	}
 }
