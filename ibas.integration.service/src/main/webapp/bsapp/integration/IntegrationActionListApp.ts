1fc8a4e47c4e24c5b16ebcc0728cb73afaeb4ee9
niurenzhu <niuren.zhu@icloud.com>
2018-01-20 16:28:59 +0800

    提示选择删除的数据

--- ibas.integration.service/src/main/webapp/bsapp/integration/IntegrationActionListApp.ts
+++ ibas.integration.service/src/main/webapp/bsapp/integration/IntegrationActionListApp.ts
@@ -141,6 +141,9 @@ export class IntegrationActionListApp extends ibas.Application<IIntegrationActio
         }
         // 没有选择删除的对象
         if (beDeleteds.length === 0) {
+            this.messages(ibas.emMessageType.WARNING, ibas.i18n.prop("shell_please_chooose_data",
+                ibas.i18n.prop("shell_data_delete")
+            ));
             return;
         }
         let that: this = this;
--- ibas.integration.service/src/main/webapp/bsapp/integrationjob/IntegrationJobListApp.ts
+++ ibas.integration.service/src/main/webapp/bsapp/integrationjob/IntegrationJobListApp.ts
@@ -134,6 +134,9 @@ export class IntegrationJobListApp extends ibas.BOListApplication<IIntegrationJo
         }
         // 没有选择删除的对象
         if (beDeleteds.length === 0) {
+            this.messages(ibas.emMessageType.WARNING, ibas.i18n.prop("shell_please_chooose_data",
+                ibas.i18n.prop("shell_data_delete")
+            ));
             return;
         }
         let that: this = this;
