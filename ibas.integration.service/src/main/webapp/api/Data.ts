77fe372b4c0f3362eb24c6415bb05897c2038cab
牛加人等于朱 <niuren.zhu@icloud.com>
2019-02-28 18:09:52 +0800

    完善前端动作类

--- ibas.integration.service/src/main/webapp/api/Data.ts
+++ ibas.integration.service/src/main/webapp/api/Data.ts
@@ -60,23 +60,35 @@ namespace integration {
         }
     }
     export namespace action {
-        /** 配置项-目标模板 */
-        export function CONFIG_TARGET(vaule: string): string {
-            return ibas.strings.format("{0}|Target", vaule);
-        }
-        export function CONFIG_SOURCE(vaule: string): string {
-            return ibas.strings.format("{0}|Source", vaule);
-        }
         /** 配置项-动作组 */
         export const CONFIG_ACTION_GROUP: string = "actionGroup";
         /** 配置项-用户 */
-        export const CONFIG_ADDRESS: string = "address";
-        /** 配置项-用户 */
-        export const CONFIG_USER: string = "user";
+        const CONFIG_USER: string = "user";
         /** 配置项-用户密码 */
-        export const CONFIG_PASSWORD: string = "password";
-        /** 配置项-用户口令 */
-        export const CONFIG_USER_TOKEN: string = "userToken";
+        const CONFIG_PASSWORD: string = "password";
+        /** 配置项-地址 */
+        const CONFIG_ADDRESS: string = "address";
+        /** 类型 */
+        export enum emSourceTarget {
+            SOURCE,
+            TARGET,
+        }
+        /** 返回配置项 */
+        export function CONFIG_KEY(type: emSourceTarget, vaule: string): string {
+            if (type === emSourceTarget.SOURCE) {
+                return ibas.strings.format("{0}|Source", vaule);
+            } else if (type === emSourceTarget.TARGET) {
+                return ibas.strings.format("{0}|Target", vaule);
+            } else {
+                return vaule;
+            }
+        }
+        /** 配置项-目标用户 */
+        export const CONFIG_USER_TARGET: string = CONFIG_KEY(emSourceTarget.TARGET, CONFIG_USER);
+        /** 配置项-目标用户密码 */
+        export const CONFIG_PASSWORD_TARGET: string = CONFIG_KEY(emSourceTarget.TARGET, CONFIG_PASSWORD);
+        /** 配置项-目标地址 */
+        export const CONFIG_ADDRESS_TARGET: string = CONFIG_KEY(emSourceTarget.TARGET, CONFIG_ADDRESS);
         /**
          * 集成动作
          */
@@ -88,6 +100,13 @@ namespace integration {
             getConfig(key: string): any;
             /**
              * 获取配置
+             * 启动乐观模式时：以“|”字符递减取值，最终未找到返回undefined
+             * @param key 配置项
+             * @param optimistic 乐观模式（仅key存在“|”时有效）
+             */
+            getConfig(key: string, optimistic: boolean): any;
+            /**
+             * 获取配置
              * @param key 配置项
              * @param defalut 默认值
              */
@@ -96,11 +115,80 @@ namespace integration {
              * 获取配置
              */
             getConfig(): any {
+                if (arguments[1] === true
+                    && typeof arguments[0] === "string" && arguments[0].indexOf("|") > 0) {
+                    // 乐观模式
+                    let key: string = typeof arguments[0];
+                    let value: any = super.getConfig(key);
+                    if (ibas.objects.isNull(value)) {
+                        key = key.substring(0, key.lastIndexOf("|"));
+                        return this.getConfig(key);
+                    }
+                    return value;
+                }
                 return super.getConfig.apply(this, arguments);
+            }
+            /**
+             * 添加配置
+             * @param key 配置项
+             * @param value 值
+             */
+            addConfig(key: string, value: any): void;
+            /**
+             * 复制配置
+             * @param action 来源
+             */
+            addConfig(action: IntegrationAction): void;
+            /**
+             * 复制配置
+             * @param action 来源
+             * @param cover 是否覆盖
+             */
+            addConfig(action: IntegrationAction, cover: boolean): void;
+            addConfig(): void {
+                if (arguments[0] instanceof IntegrationAction) {
+                    let cover: boolean = arguments[1];
+                    if (ibas.objects.isNull(cover)) {
+                        cover = true;
+                    }
+                    ibas.actions.copyConfig(arguments[0], this, cover);
+                } else {
+                    super.addConfig.apply(this, arguments);
+                }
+            }
+            /** 设置日志记录者 */
+            setLogger(logger: ibas.ILogger): void;
+            /** 设置日志记录者 */
+            setLogger(action: IntegrationAction): void;
+            /** 设置日志记录者 */
+            setLogger(): void {
+                if (arguments[0] instanceof IntegrationAction) {
+                    ibas.actions.copyLogger(arguments[0], this);
+                } else {
+                    super.setLogger.apply(this, arguments);
+                }
             }
             /** 运行 */
             protected run(): boolean {
-                return this.execute();
+                // 加载资源文件
+                for (let item of ibas.arrays.create(this.resources())) {
+                    if (!ibas.strings.isWith(item, "http", undefined)) {
+                        let url: string = this.getConfig(CONFIG_ACTION_GROUP);
+                        if (!ibas.strings.isWith(url, undefined, "/")
+                            && !ibas.strings.isWith(item, "/", undefined)) {
+                            url = url + "/";
+                        }
+                        ibas.i18n.load(url + item);
+                    } else {
+                        ibas.i18n.load(item);
+                    }
+                }
+                // 执行逻辑
+                let value: any = this.execute();
+                if (typeof value === "boolean") {
+                    return value;
+                }
+                return false;
             }
             /** 调用后台服务 */
             protected goAction(name: string, params: { key: string, value: any }[] | { key: string, value: any }, fnBack?: (opRslt: ibas.IOperationMessage) => void): void {
@@ -126,8 +214,189 @@ namespace integration {
                     }
                 });
             }
+            /** 资源文件（可重载） */
+            protected resources(): string[] | string {
+                return undefined;
+            }
             /** 运行（需要实现） */
-            protected abstract execute(): boolean;
+            protected abstract execute(): boolean | void;
+        }
+        /**
+         * 获取仓库名称（去除Async结尾）
+         * @param boRepository 业务仓库实例或名称
+         */
+        export function repositoryName(boRepository: ibas.BORepositoryApplication | string): string {
+            let name: string;
+            if (boRepository instanceof ibas.BORepositoryApplication) {
+                name = ibas.objects.getTypeName(boRepository);
+            } else {
+                name = ibas.strings.valueOf(boRepository);
+            }
+            if (ibas.strings.isWith(name, undefined, "Async")) {
+                name = name.substring(0, name.length - "Async".length);
+            }
+            return name;
+        }
+        /**
+         * 根据集成动作配置改变业务仓库信息
+         * @param boRepository 业务仓库
+         * @param action 集成动作
+         * @param type 类型
+         */
+        export function changeRepositoryInfo(boRepository: ibas.BORepositoryApplication,
+            action: IntegrationAction, type: emSourceTarget = emSourceTarget.TARGET): void {
+            if (!(boRepository instanceof ibas.BORepositoryApplication)) {
+                return;
+            }
+            // 获取业务仓库名称
+            let name: string = repositoryName(boRepository);
+            if (ibas.objects.isNull(action)) {
+                // 没有提供配置，则从全局config中获取
+                ibas.BORepositoryApplication.repositoryInfo(boRepository, name);
+            }
+            if (!(action instanceof IntegrationAction)) {
+                return;
+            }
+            // 目标类型，尝试先补全地址
+            if (type === emSourceTarget.TARGET) {
+                completeAddress(action, boRepository);
+            }
+            let configKey: string, configValue: any;
+            // 地址
+            configKey = CONFIG_KEY(type,
+                ibas.strings.format(ibas.CONFIG_ITEM_TEMPLATE_REMOTE_REPOSITORY_ADDRESS, name));
+            configValue = action.getConfig(configKey, true);
+            if (!ibas.objects.isNull(configValue)) {
+                boRepository.address = configValue;
+                ibas.logger.log(ibas.emMessageLevel.DEBUG, "action: [{0}] changed address [{1}].", ibas.objects.getTypeName(boRepository), boRepository.address);
+            }
+            // 口令
+            configKey = CONFIG_KEY(type,
+                ibas.strings.format(ibas.CONFIG_ITEM_TEMPLATE_USER_TOKEN, name));
+            configValue = action.getConfig(configKey, true);
+            if (!ibas.objects.isNull(configValue)) {
+                boRepository.token = configValue;
+                ibas.logger.log(ibas.emMessageLevel.DEBUG, "action: [{0}] changed token [{1}].", ibas.objects.getTypeName(boRepository), boRepository.token);
+            }
+            // 离线
+            configKey = CONFIG_KEY(type,
+                ibas.strings.format(ibas.CONFIG_ITEM_TEMPLATE_OFFLINE_MODE, name));
+            configValue = action.getConfig(configKey, true);
+            if (!ibas.objects.isNull(configValue)) {
+                boRepository.offline = configValue;
+                ibas.logger.log(ibas.emMessageLevel.DEBUG, "action: [{0}] changed offline [{1}].", ibas.objects.getTypeName(boRepository), boRepository.offline);
+            }
+        }
+        /**
+         * 连接业务仓库
+         * @param user 用户
+         * @param password 密码
+         * @param onCompleted 完成
+         */
+        export function connectRepository(user: string, password: string, onCompleted: (opRslt: ibas.IOperationResult<shell.bo.IUser>) => void): void;
+        /**
+         * 连接业务仓库（token写入集成动作配置）
+         * @param action 集成动作
+         * @param type 类型
+         */
+        export function connectRepository(action: IntegrationAction, onCompleted: (opRslt: ibas.IOperationResult<shell.bo.IUser>) => void): void;
+        export function connectRepository(): void {
+            let user: string, password: string, address: string, onCompleted: Function;
+            let action: IntegrationAction, type: emSourceTarget = emSourceTarget.TARGET;
+            if (arguments.length === 3
+                && typeof arguments[0] === "string"
+                && typeof arguments[1] === "string"
+                && typeof arguments[2] === "function") {
+                // 用户密码直接连接
+                user = arguments[0];
+                password = arguments[1];
+                onCompleted = arguments[2];
+            } else if (arguments[0] instanceof IntegrationAction) {
+                action = arguments[0];
+                onCompleted = arguments[1];
+                user = action.getConfig(CONFIG_USER_TARGET, true);
+                password = action.getConfig(CONFIG_PASSWORD_TARGET, true);
+                address = action.getConfig(CONFIG_KEY(type, shell.bo.BO_REPOSITORY_CONNECT));
+                if (ibas.strings.isEmpty(address)) {
+                    // 不存在目标地址，用address完善
+                    address = action.getConfig(CONFIG_ADDRESS_TARGET, true);
+                    if (!ibas.strings.isEmpty(address)) {
+                        if (ibas.strings.isWith(address, undefined, "/")) {
+                            address = address + "/";
+                        }
+                        address = address + "initialfantasy/services/rest/data/";
+                        action.addConfig(CONFIG_KEY(type, shell.bo.BO_REPOSITORY_CONNECT), address);
+                    }
+                }
+            } else {
+                throw new Error(ibas.i18n.prop("sys_invalid_parameter", "all"));
+            }
+            if (ibas.strings.isEmpty(user)) {
+                throw new Error(ibas.i18n.prop("sys_invalid_parameter", "user"));
+            }
+            if (ibas.strings.isEmpty(password)) {
+                throw new Error(ibas.i18n.prop("sys_invalid_parameter", "password"));
+            }
+            let boRepository: shell.bo.IBORepositoryConnect = shell.bo.boFactory.create(shell.bo.BO_REPOSITORY_CONNECT);
+            if (!ibas.strings.isEmpty(address)) {
+                boRepository.address = address;
+            }
+            boRepository.userConnect({
+                user: user,
+                password: password,
+                onCompleted: (opRslt) => {
+                    if (action instanceof IntegrationAction) {
+                        let user: shell.bo.IUser = opRslt.resultObjects.firstOrDefault();
+                        if (!ibas.objects.isNull(user)) {
+                            action.addConfig(CONFIG_KEY(type, ibas.CONFIG_ITEM_USER_TOKEN), user.token);
+                        }
+                    }
+                    if (onCompleted instanceof Function) {
+                        onCompleted(opRslt);
+                    }
+                }
+            });
+        }
+        /**
+         * 补全业务仓库地址（根据address配置，仅适用Target系统）
+         * @param action 集成动作
+         * @param boRepository 业务仓库实例或名称
+         */
+        export function completeAddress(action: IntegrationAction, boRepository: ibas.BORepositoryApplication | string): string {
+            if (!(action instanceof IntegrationAction)) {
+                return undefined;
+            }
+            let name: string = repositoryName(boRepository);
+            // 已存在仓库目标地址，则退出
+            let address: string = action.getConfig(
+                CONFIG_KEY(emSourceTarget.TARGET,
+                    ibas.strings.format(ibas.CONFIG_ITEM_TEMPLATE_REMOTE_REPOSITORY_ADDRESS, name))
+            );
+            if (!ibas.strings.isEmpty(address)) {
+                return address;
+            }
+            // 不存在目标地址，用address完善
+            address = action.getConfig(CONFIG_ADDRESS_TARGET, true);
+            if (ibas.strings.isEmpty(address)) {
+                return undefined;
+            }
+            let url: ibas.StringBuilder = new ibas.StringBuilder();
+            url.append(address);
+            if (ibas.strings.isWith(address, undefined, "/")) {
+                url.append("/");
+            }
+            if (name.startsWith("BORepository")) {
+                url.append(name.substring("BORepository".length).toLowerCase());
+            } else {
+                url.append(name.toLowerCase());
+            }
+            url.append("/services/rest/data/");
+            address = url.toString();
+            action.addConfig(CONFIG_KEY(emSourceTarget.TARGET,
+                ibas.strings.format(ibas.CONFIG_ITEM_TEMPLATE_REMOTE_REPOSITORY_ADDRESS, name)
+            ), address);
+            ibas.logger.log(ibas.emMessageLevel.DEBUG, "action: register [{0}]'s address [{1}].", ibas.objects.getTypeName(boRepository), address);
+            return address;
         }
     }
 }
